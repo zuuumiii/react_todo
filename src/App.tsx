c2129238d81ed8a753f08977a6969ce125cf5541
Keisuke Kitazumi <kitazumi.gc8@gmail.com>
2022-03-15 11:03:12 +0900

    add State

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,38 @@ import { useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
-export const App = () => {
-  const [count, setCount] = useState(0);
+type Todo = {
+  value: string;
+};
 
+export const App = () => {
+  const [text, setText] = useState("");
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const handleOnSubimt = () => {
+    if (!text) return;
+
+    const newTodo: Todo = {
+      value: text,
+    };
+
+    setTodos([newTodo, ...todos]);
+    setText("");
+  };
   return (
     <div>
-      <form onSubmit={(e) => e.preventDefault()}>
-        <input type="text" value="" onChange={(e) => e.preventDefault()} />
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          handleOnSubimt();
+        }}
+      >
         <input
-          type="submit"
-          value="追加"
-          onSubmit={(e) => e.preventDefault()}
+          type="text"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
         />
+        <input type="submit" value="追加" onSubmit={handleOnSubimt} />
       </form>
     </div>
   );
